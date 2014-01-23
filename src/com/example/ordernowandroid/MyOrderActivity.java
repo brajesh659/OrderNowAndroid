621f6cfaac6533cc7aefc5b63b8a7ae11116b0dd
Brajesh <braj.upad@gmail.com>
2014-01-23 06:05:32 +0530

    UpNavigation for MyOrder should display correct view

--- src/com/example/ordernowandroid/MyOrderActivity.java
+++ src/com/example/ordernowandroid/MyOrderActivity.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -94,6 +95,19 @@ public class MyOrderActivity extends Activity {
         MyOrderAdapter myOrderAdapter = new MyOrderAdapter(MyOrderActivity.this, myOrders);
         myOrderListView.setAdapter(myOrderAdapter);
     }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	
+    	switch (item.getItemId()) {
+        // Respond to the action bar's Up/Home button
+        case android.R.id.home:
+            finish();
+            return true;
+        }
+    	return super.onOptionsItemSelected(item);
+    }
+    
     @Override
     public void finish() {
         Intent data = new Intent();
