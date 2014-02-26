d46611c5a7558367637f980afa2a22ad0694a3bb
rohitgupta4 <rohitnsit.1989@gmail.com>
2014-02-26 17:31:44 +0530

    Display Item Note on My Order Page

--- src/com/example/ordernowandroid/adapter/MyOrderAdapter.java
+++ src/com/example/ordernowandroid/adapter/MyOrderAdapter.java
@@ -6,6 +6,7 @@ import java.util.List;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -29,13 +30,15 @@ import com.example.ordernowandroid.model.OrderNowConstants;
 
 public class MyOrderAdapter extends ArrayAdapter<MyOrderItem> {
 
-    private ApplicationState applicationState;
-    List<MyOrderItem> myOrderItemList ; 
+	private ApplicationState applicationState;
+	private static final String TEXT_COMMENT = "TextComment";
+
+	List<MyOrderItem> myOrderItemList ; 
 	public MyOrderAdapter(Context context, List<MyOrderItem> myOrderItemList) {
-	    super(context, R.layout.my_order, myOrderItemList);
+		super(context, R.layout.my_order, myOrderItemList);
 		applicationState = (ApplicationState) context.getApplicationContext();
 		this.myOrderItemList = myOrderItemList;
-		
+
 	}
 
 	@Override
@@ -45,15 +48,26 @@ public class MyOrderAdapter extends ArrayAdapter<MyOrderItem> {
 			convertView = vi.inflate(R.layout.my_order, null);
 		}
 
-		TextView itemName = (TextView) convertView.findViewById(R.id.itemName);		
+		TextView itemName = (TextView) convertView.findViewById(R.id.itemName);
+		TextView itemNote = (TextView) convertView.findViewById(R.id.itemNote);
 		final TextView quantity = (TextView) convertView.findViewById(R.id.quantity);
 		final TextView itemTotalPrice = (TextView) convertView.findViewById(R.id.itemTotalPrice);
-		
+
 		String orderItemName = myOrderItemList.get(position).getFoodMenuItem().getItemName();
+		String orderItemNote = null;
+		if (myOrderItemList.get(position).getMetaData() != null) {
+			orderItemNote = myOrderItemList.get(position).getMetaData().get(TEXT_COMMENT);
+		}
 		Float orderItemQuantity = myOrderItemList.get(position).getQuantity();
 		Float orderItemPrice = myOrderItemList.get(position).getFoodMenuItem().getItemPrice();		
 
-		itemName.setText(orderItemName);	
+		itemName.setText(orderItemName);
+		if(orderItemNote != null) {
+			itemNote.setText(orderItemNote);
+		} else {
+			itemNote.setVisibility(View.GONE);
+			itemName.setGravity(Gravity.CENTER_VERTICAL);	
+		}
 		quantity.setText(Float.toString(orderItemQuantity));
 		itemTotalPrice.setText(OrderNowConstants.INDIAN_RUPEE_UNICODE + " " + Float.toString(orderItemPrice * orderItemQuantity));
 
@@ -63,7 +77,7 @@ public class MyOrderAdapter extends ArrayAdapter<MyOrderItem> {
 		decrementQtyBtn.setOnClickListener(new OnClickListener() { 
 			@Override
 			public void onClick(View v) {
-			    HashMap<String, MyOrderItem> foodMenuItemQuantityMap = ApplicationState.getFoodMenuItemQuantityMap(applicationState);
+				HashMap<String, MyOrderItem> foodMenuItemQuantityMap = ApplicationState.getFoodMenuItemQuantityMap(applicationState);
 				Float qty = Float.parseFloat((String) quantity.getText());
 				String orderItemPriceStr = (String) itemTotalPrice.getText();				
 
@@ -71,14 +85,14 @@ public class MyOrderAdapter extends ArrayAdapter<MyOrderItem> {
 					orderItemPriceStr = orderItemPriceStr.substring(orderItemPriceStr.indexOf(OrderNowConstants.INDIAN_RUPEE_UNICODE) + 1).trim();
 				}
 				final Float orderItemPrice = Float.parseFloat(orderItemPriceStr);
-				
+
 				RelativeLayout r = (RelativeLayout) ((ViewGroup) parent).getParent(); // This is to get the Parent View of the List View
 				final TextView orderTotalPriceView = (TextView) r.findViewById(R.id.relativeBtnLayout).findViewById(R.id.totalAmount);
-				
+
 				if (qty > 1) {
 					itemTotalPrice.setText(OrderNowConstants.INDIAN_RUPEE_UNICODE + " " + Float.toString((orderItemPrice/qty) * (qty - 1)));
 					quantity.setText(Float.toString(qty - 1));					
-					
+
 					String orderTotalPriceStr = (String) orderTotalPriceView.getText();
 					if (orderTotalPriceStr.indexOf(OrderNowConstants.INDIAN_RUPEE_UNICODE) != -1){
 						orderTotalPriceStr = orderTotalPriceStr.substring(orderTotalPriceStr.indexOf(OrderNowConstants.INDIAN_RUPEE_UNICODE) + 1).trim();
@@ -87,7 +101,7 @@ public class MyOrderAdapter extends ArrayAdapter<MyOrderItem> {
 					orderTotalPriceView.setText(OrderNowConstants.INDIAN_RUPEE_UNICODE + " " + Float.toString(newOrderTotalPrice));					
 					myOrderItemList.get(position).setQuantity(qty - 1);	
 					foodMenuItemQuantityMap.get(myOrderItemList.get(position).getFoodMenuItem().getItemName()).setQuantity(qty-1);
-					
+
 				} else if (qty == 1){
 					//Show Dialog and Remove Item from ListView on Positive Button Action
 					AlertDialog.Builder builder = new AlertDialog.Builder(getContext());            
@@ -96,8 +110,8 @@ public class MyOrderAdapter extends ArrayAdapter<MyOrderItem> {
 					builder.setPositiveButton(R.string.ok, new AlertDialog.OnClickListener() {
 						@Override
 						public void onClick(DialogInterface dialog, int which) {
-			                HashMap<String, MyOrderItem> foodMenuItemQuantityMap = ApplicationState.getFoodMenuItemQuantityMap(applicationState);
-						    foodMenuItemQuantityMap.remove(getItem(position).getFoodMenuItem().getItemName());
+							HashMap<String, MyOrderItem> foodMenuItemQuantityMap = ApplicationState.getFoodMenuItemQuantityMap(applicationState);
+							foodMenuItemQuantityMap.remove(getItem(position).getFoodMenuItem().getItemName());
 							remove(getItem(position));
 							notifyDataSetChanged();
 							String orderTotalPriceStr = (String) orderTotalPriceView.getText();
@@ -108,8 +122,8 @@ public class MyOrderAdapter extends ArrayAdapter<MyOrderItem> {
 							orderTotalPriceView.setText(OrderNowConstants.INDIAN_RUPEE_UNICODE + " " + Float.toString(newOrderTotalPrice));
 						}
 					}
-					
-					);
+
+							);
 
 					builder.setNegativeButton(R.string.cancel, null);	            	            
 					AlertDialog alert = builder.create();
@@ -117,7 +131,7 @@ public class MyOrderAdapter extends ArrayAdapter<MyOrderItem> {
 				} else {
 					Toast.makeText(getContext(), "Quantity cannnot be decreased below zero", Toast.LENGTH_SHORT).show();
 				}
-				
+
 			}
 		});
 
@@ -131,16 +145,16 @@ public class MyOrderAdapter extends ArrayAdapter<MyOrderItem> {
 					orderItemPriceStr = orderItemPriceStr.substring(orderItemPriceStr.indexOf(OrderNowConstants.INDIAN_RUPEE_UNICODE) + 1).trim();
 				}
 				Float orderItemPrice = Float.parseFloat(orderItemPriceStr);
-				
+
 				RelativeLayout r = (RelativeLayout) ((ViewGroup) parent).getParent(); // This is to get the Parent View of the List View
 				final TextView orderTotalPriceView = (TextView) r.findViewById(R.id.relativeBtnLayout).findViewById(R.id.totalAmount);
-				
+
 				if (qty == 0){
 					Toast.makeText(getContext(), "Cannot determine Unit Price if the Quantity is zero", Toast.LENGTH_SHORT).show();					
 				} else {
 					itemTotalPrice.setText(OrderNowConstants.INDIAN_RUPEE_UNICODE + " " + Float.toString((orderItemPrice/qty) * (qty + 1)));
 					quantity.setText(Float.toString(qty + 1));					
-					
+
 					String orderTotalPriceStr = (String) orderTotalPriceView.getText();
 					if (orderTotalPriceStr.indexOf(OrderNowConstants.INDIAN_RUPEE_UNICODE) != -1){
 						orderTotalPriceStr = orderTotalPriceStr.substring(orderTotalPriceStr.indexOf(OrderNowConstants.INDIAN_RUPEE_UNICODE) + 1).trim();
