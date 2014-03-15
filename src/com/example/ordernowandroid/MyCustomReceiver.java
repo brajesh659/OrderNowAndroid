e1d352d5047d6f9724164380f544b39906ac3c07
sjain0@walmartlabs.com <siddhanth.iitb@gmail.com>
2014-03-15 20:27:59 +0530

    Handling Multiple notifications

--- src/com/example/ordernowandroid/MyCustomReceiver.java
+++ src/com/example/ordernowandroid/MyCustomReceiver.java
@@ -5,14 +5,12 @@ import java.util.Iterator;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
@@ -58,26 +56,25 @@ public class MyCustomReceiver extends BroadcastReceiver {
             String channel = intent.getExtras().getString("com.parse.Channel");
             JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
 
+			System.out.println(json);
             Iterator itr = json.keys();
            // Toast.makeText(context, "MyCustomerReciver", duration).show();
-
+			String message = null;
+			String subject = "";
             while (itr.hasNext()) {
                 String key = (String) itr.next();
-                String message = null;
-                String subject = "";
                 if (key.equals("message")) {
                     message = json.getString(key);
                     //toast = Toast.makeText(context, message, duration);
                     //toast.show();
-                }
-                if (key.equals("dishIds")) {
+				} else if (key.equals("dishIds")) {
                     subject = json.getString(key);
                     //toast = Toast.makeText(context, subject, duration);
                     //toast.show();
-                }
+				}
+			}
+			notification(context, message, subject);
 
-                notification(context, message, subject);
-            }
         } catch (JSONException e) {
         }
     }
